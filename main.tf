4c65afd1876da699d4986b772638054792629247
satyendra <satyendra10067@gmail.com>
2025-07-22 09:54:21 +0530

    one more rg add

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ resource "azurerm_resource_group" "raju" {
   name     = "raju_rg"
   location = "westeurope"
 }
+resource "azurerm_resource_group" "raju1" {
+  name     = "raju_rg1"
+  location = "westeurope"
+}
